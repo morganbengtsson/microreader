a2347cbb68d9e779ceb2bc2c42a80f156364f861
mobe <morgan.bengtsson@hb.se>
2013-03-26 14:34:50 +0100

    Simple channel listing.

--- rss.py
+++ rss.py
@@ -1,31 +1,35 @@
-import feedparser, sqlite3, bottle
-import lxml.html
+import feedparser, bottle
+import xml.etree.ElementTree as ET
 from bottle import route, run, view, SimpleTemplate
 
 @route('/')
 def index():
 	return "test"
 
-@route('/items')
-def items():
-	conn = sqlite3.connect('database.db')
-	c = conn.cursor()
-
-@route('/rss')
-@view('channels')
-@route('/api/rss')
-def channels():
-	feed = feedparser.parse('http://rss.slashdot.org/Slashdot/slashdot')
-	
-	name_map = {'title' : 'title'}
+@route("/<url:re:.+>")
+@route('/api/<url:re:.+>')
+@view('feed')
+# TODO: strip HTML tags
+def feed(url = ''):	
+	feed = feedparser.parse(url)	
 	items = {'items' : []}
 	for item in feed.entries:
 		items['items'].append({'title': item.title, 
-							   'description' : lxml.html.fromstring(item.description).text_content(),
-							   'link' : item.link})
-	
+							   'description' : item.description,
+							   'link' : item.link})	
 	return items
 	
+@route('/')
+@route('/api')
+@view('channels')
+def channels():
+	tree = ET.parse('subscriptions.xml')
+	channels = {'channels' : []}
+	print (tree.getroot().findall('./body/outline'))
 		
+	for channel in tree.getroot().findall('./body/outline'):
+		channels['channels'].append({'title' : channel.get('title'), 'url' : channel.get('xmlUrl')})
+	
+	return channels
 
 run(host='localhost', port=3000, reloader = True, debug = True)
