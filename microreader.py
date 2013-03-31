3f6dbebdd7a74406a52875ca57b848da41e1b58d
Morgan Bengtsson <bengtsson.morgan@gmail.com>
2013-03-31 13:11:59 +0200

    Cleanup.

--- microreader.py
+++ microreader.py
@@ -35,7 +35,7 @@ Channel.create_table(fail_silently = True)
 Item.create_table(fail_silently = True)
 db.close()
 
-@route('/api/<url:re:.+>')
+@route('/api/items/<url:re:.+>')
 def items(url = ''):
 	items = {'items' : [], 'url' : url}
 	
@@ -54,16 +54,12 @@ def items(url = ''):
 			#i = Item.create(title = item.title, description = item.description, url = item.link)
 	return items
 
-@route('/api')
+@route('/api/channels')
 def channels():
-	tree = ET.parse('subscriptions.xml')
-	#for channel in tree.getroot().findall('./body/outline'):		
-		#Channel.create(title = channel.get('title'), url = channel.get('xmlUrl'))
-			
 	return {'channels' : [c for c in Channel.select().dicts()]}
 	
-@route("/home")
-@route("/home/<url:re:.+>")
+@route("/channels")
+@route("/channels/<url:re:.+>")
 @view('index')
 def index(url = ''):	
 	index = dict(items(url),**channels())
